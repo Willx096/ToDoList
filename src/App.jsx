f2c3beb7b5cd4763b33816fac27c0dac924cd083
Will096 <guillem.lorca@gmail.com>
2023-03-21 18:36:52 +0100

    Tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,85 @@
 import { useState } from 'react'
 import './App.css'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
+import { Container, Row, Col ,Form, Button,Table } from "react-bootstrap";
+
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
+import {faEnvelope, faPhone, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState ("");
+  const [item, setItem] = useState("");
+  const [lista, setLista] = useState([""]);
+
+  function borraItem(x){
+    const nuevaLista = [...lista]
+    nuevaLista.splice(x,1)
+    setLista(nuevaLista)
+  }
+
+  //<li></li>
+  const listaItems = lista.map((el, index )=> <li key={index}>{el}</li>)
+  const listaFilas = lista.map((el, index )=> 
+  <tr key={index}><td>
+    {index+1}</td>
+    <td>{el}</td>
+    <td><Button   variant="danger" onClick={()=>borraItem(index)}><FontAwesomeIcon  icon={faTrashCan}/></Button>
+    <span className="papelera">
+    </span>
+    
+    </td>
+    
+    </tr>)
+
+
+
+  function nuevoItem(){
+      const nuevaLista = [...lista];
+      nuevaLista.push(item);
+      setLista(nuevaLista);
+      setItem("");
+  }
   
-
-  const [errorPassword, setErrorPassword] = useState(false)
-
-  function enviar (e){
-    // e.preventDefault();
-
-    console.log(email, password)
-    if (!password.includes("$")){
-      setErrorPassword(true);
-
-    }else{
-      setErrorPassword(false)
-    }
-   
+  function limpiaLista(){
+    setLista([]);
   }
   
   return (
-          <Container>         
+    <>
+     <Container>
+     <h1>To Do List</h1>   
+     <Row>
+      <Col> 
+      <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Entrar Valor :</Form.Label>
+        <Form.Control type="text" value={item} onInput={e=> setItem(e.target.value)} />
+      </Form.Group>
+      <Button variant="success" onClick={nuevoItem}>Agregar</Button>      
+      </Form>
+      </Col>
+      <Col>
+      <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Item</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {listaFilas}      
+      </tbody>      
+    </Table>
+    <Button variant="danger" onClick={limpiaLista}>Clear</Button>
 
-
-
-            <Row>
-              <h1>Formulario {email}+{password}</h1>
-            <Col>
-            <Form onSubmit={enviar}>
-            <Form.Group className="mb-" controlId="formBasicEmail">
-        <Form.Label>Nombre</Form.Label>
-        <Form.Control type="name" placeholder="Nombre"/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Apellido</Form.Label>
-        <Form.Control type="Apellido" placeholder="Apellido"/>
-        </Form.Group>       
-        <Row>
-        <Col>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Dirección</Form.Label>
-        <Form.Control type="direccion" placeholder="Dirección"/>
-        </Form.Group>
-        </Col>
-        <Col>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>C.Postal</Form.Label>
-        <Form.Control type="CP" placeholder="C.Postal"/>
-        </Form.Group>   
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-        <Form.Check
-           label ="Menos de 25 años"
-           name='group1'
-           type="radio"
-          />
-          <Form.Check
-           label ="26 a 40 años"
-           name='group1'
-           type="radio"
-          /><Form.Check
-          label ="41 a 65 años"
-          name='group1'
-          type="radio"
-         />
-         <Form.Check
-           label ="Más de 66 años"
-           name='group1'
-           type="radio"
-          />
-        </Col>
-        <Col> 
-        <FontAwesomeIcon icon={faPhone} />       
-        <Form.Label>Telefono móbil</Form.Label>
-        <Form.Select aria-label="Default select example">            
-      <option>Open this select menu</option>
-      <option value="1">No tiene</option>
-      <option value="2">Si tiene</option>
-      </Form.Select>
-      <Form.Label>Vehiculo</Form.Label>
-        <Form.Select aria-label="Default select example">            
-      <option>Open this select menu</option>
-      <option value="1">No tiene</option>
-      <option value="2">Si tiene</option>
-    </Form.Select>        
-        </Col>
-        
-      </Row>
-      
-
-      <Button variant="primary" type="submit">
-        Enviar
-      </Button>
-    </Form>
-           
-            </Col>           
-           </Row>      
-        
-          </Container>
-  )
+      </Col>
+      </Row>          
+     </Container>
+     </>
+)
+    
+         
 }
 
 export default App
